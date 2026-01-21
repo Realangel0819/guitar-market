3bc1c627700904b64e6fc202ca3e70bab198a781
Realangel0819 <dlrlwk010@naver.com>
2026-01-21 18:59:17 +0900

    chore: initial project setup

---
+++ src/main/java/com/guitarmarket/GuitarMarketApplication.java
@@ -0,0 +1,13 @@
+package com.guitarmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarMarketApplication.class, args);
+	}
+
+}
